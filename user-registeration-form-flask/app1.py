08996ca1b6a89e51691442ed94da86ccf3f91ccd
Shivamtayal99 <38536596+Shivamtayal99@users.noreply.github.com>
2020-09-30 10:58:17 +0530

    Add files via upload

---
+++ user-registeration-form-flask/app1.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+import mysql.connector
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+    return render_template('index.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="register"
+    )
+    mycursor=mydb.cursor()
+    if request.method=='POST':
+        signup=request.form
+        username=signup['user']
+        email=signup['mail']
+        mobile=signup['mob']
+        password=signup['pass']
+        rpassword=signup['rpass']
+        mycursor.execute("insert into reg(user,email,mob,pass,rpass)values(%s,%s,%s,%s,%s)",(username,email,mobile,password,rpassword))
+        mydb.commit()
+        mycursor.close()
+        return "Registeration Successfull"
+
+app.run(debug=True)
